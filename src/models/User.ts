b3be655a0fe688dc8c18522b97978037c3e28705
Slava <kreeon@ya.ru>
2020-04-19 17:11:33 +0300

    extract attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Sync } from './Sync';
 import { Eventing } from './Eventing';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -7,16 +8,9 @@ export interface UserProps {
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
   public events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
