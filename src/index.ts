55706ec76b7d3e000466b93c49270d1c5879967d
Slava <kreeon@ya.ru>
2020-04-19 13:53:03 +0300

    integrate events to user

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ age: 1, name: 'Another' });
 
-user.save();
+user.events.on('change', () => {
+  console.log('Change');
+});
+
+user.events.trigger('change');
