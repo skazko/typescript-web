2b3bfec1fd2d2cb68494170156fab51061114d99
Slava <kreeon@ya.ru>
2020-04-20 17:50:12 +0300

    add UserForm

---
+++ src/views/UserForm.ts
@@ -0,0 +1,65 @@
+import { User } from '../models/User';
+import { textSpanIsEmpty } from 'typescript';
+
+export class UserForm {
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+    };
+  }
+
+  template(): string {
+    return `
+      <div>
+        <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
+        <input />
+        <button class="set-name">Change name</button>
+        <button class="set-age">Set random age</button>
+      </div>
+    `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach((el: Element) => {
+        el.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    this.parent.innerHTML = '';
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+    this.parent.append(templateElement.content);
+  }
+}
