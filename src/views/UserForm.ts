6fa28f73172f0c92e09e6aad7f65329889cf5c1e
Slava <kreeon@ya.ru>
2020-04-20 18:34:37 +0300

    add save button to User Form

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,22 +14,25 @@ export class UserForm extends View<User, UserProps> {
     }
   };
 
+  onSaveClick = (): void => {
+    this.model.save();
+  };
+
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
 
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
-        <input />
+        <input placeholder="${this.model.get('name')}" />
         <button class="set-name">Change name</button>
         <button class="set-age">Set random age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
